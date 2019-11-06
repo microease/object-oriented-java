d35b5888e6cdabe05f1f61e2c5dabff320b69938
浮生若梦 <microease@163.com>
2019-11-06 21:35:53 +0800

    add

---
+++ week2/SimpleLocation.java
@@ -0,0 +1,14 @@
+public class SimpleLocation {
+    //经纬度 成员变量
+    public double latitude;
+    public double longitude;
+    public SimpleLocation(double lat,double lon){
+        this.latitude = lat;
+        this.longitude = lon;
+    }
+    public double distance(SimpleLocation other){
+        //
+    }
+
+
+}
