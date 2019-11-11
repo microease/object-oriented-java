8e15235fcd7a3b42fd03c689781f70e904853022
浮生若梦 <microease@163.com>
2019-11-12 00:06:30 +0800

    add

---
+++ week2/LocationTester.java
@@ -0,0 +1,11 @@
+
+public class LocationTester
+{
+    public static void main(String[] args)
+    {
+        double latitude = -15.5;
+        SimpleLocation lima = new SimpleLocation(latitude, -77.0);
+        latitude = -12.0;
+        System.out.println(lima.latitude);
+    }
+}
